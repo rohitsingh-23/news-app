d6844c00d5a6c65640fc650ee1bca52bd1e1abab
Rohit Kumar Singh <93372627+rohitsingh-23@users.noreply.github.com>
2022-02-11 23:34:40 +0530

    News-app

---
+++ components/sidebar.js
@@ -0,0 +1,15 @@
+function nav(){
+    return `<h2 id="home">Home</h2>
+    <h2>Login</h2>
+    <h2>Audios</h2>
+    <h2>Videos</h2>
+    <h2>Newsletters</h2>
+    <h2>Event</h2>
+    <h2>More</h2>
+    <div>
+        <input id="input" type="text" placeholder="Search">
+        <button id="search" >Search</button>
+    </div>`
+}
+
+export default nav;
